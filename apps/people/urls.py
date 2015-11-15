56fd28fe5bd252017e4100ce189937a3e4d1099d
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-15 20:48:14 +0000

    Switch to the new URLs.py style. Ref onespacemedia/cms#80.

--- apps/people/urls.py
+++ apps/people/urls.py
@@ -1,27 +1,11 @@
 """ URLs used by the people app """
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 import views
 
-urlpatterns = patterns('',
-                       url(
-                           r"^teams/$",
-                           views.TeamListView.as_view(),
-                           name='team_list'
-                       ),
-                       url(
-                           r"^team/(?P<team_title>[a-z-]+)/$",
-                           views.TeamView.as_view(),
-                           name='team'
-                       ),
-                       url(
-                           r"^$",
-                           views.PersonListView.as_view(),
-                           name="person_list"
-                       ),
-                       url(
-                           r"^(?P<person_title>[a-z-]+)/$",
-                           views.PersonView.as_view(),
-                           name='person'
-                       )
-)
+urlpatterns = [
+    url(r"^teams/$", views.TeamListView.as_view(), name='team_list'),
+    url(r"^team/(?P<team_title>[a-z-]+)/$", views.TeamView.as_view(), name='team'),
+    url(r"^$", views.PersonListView.as_view(), name="person_list"),
+    url(r"^(?P<person_title>[a-z-]+)/$", views.PersonView.as_view(), name='person')
+]
