f21e11cf527cac8b4e6953072c328b9c74579ac0
Dan Gamble <dangamble89@gmail.com>
2017-06-21 13:51:38 +0100

    Move import in to apps.ready()

--- apps/people/apps.py
+++ apps/people/apps.py
@@ -1,4 +1,3 @@
-from cms.models import PageBaseSearchAdapter
 from django.apps import AppConfig
 from watson import search as watson
 
@@ -7,5 +6,7 @@ class PeopleConfig(AppConfig):
     name = '{{ project_name }}.apps.people'
 
     def ready(self):
+        from cms.models import PageBaseSearchAdapter
+
         Person = self.get_model('Person')
         watson.register(Person, adapter_cls=PageBaseSearchAdapter)
